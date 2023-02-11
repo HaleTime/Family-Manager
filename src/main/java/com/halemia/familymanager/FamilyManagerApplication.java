5b0071fafae156efed1cd18651ab842b9f8eb7f7
hale <shangchy_s@163.com>
2023-02-12 01:30:30 +0800

    first commit

---
+++ src/main/java/com/halemia/familymanager/FamilyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.halemia.familymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FamilyManagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FamilyManagerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/halemia/familymanager/FamilyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.halemia.familymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FamilyManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
